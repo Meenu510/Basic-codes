8ebde233be5928dd91d61cc71579d1595c7c11b7
Meenu510 <89180244+Meenu510@users.noreply.github.com>
2023-08-12 23:53:46 +0530

    Create Day 12(Balance parenthesis).py

---
+++ Day 12(Balance parenthesis).py
@@ -0,0 +1,17 @@
+def is_balanced(s):
+    stack = []
+    mapping = {")": "(", "}": "{", "]": "["}
+    
+    for char in s:
+        if char in mapping:
+            top_element = stack.pop() if stack else '#'
+            
+            if mapping[char] != top_element:
+                return False
+        else:
+            stack.append(char)
+    
+    return not stack
+
+S = "{[()]}"
+print(is_balanced(S))  # This should return True
