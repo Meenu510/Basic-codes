c3d7d22521d4be9aa7e974d80578d7bfd5c13e4b
Meenu510 <89180244+Meenu510@users.noreply.github.com>
2023-08-13 18:03:56 +0530

    Create Day13(Roman to decimal).py

---
+++ Day13(Roman to decimal).py
@@ -0,0 +1,29 @@
+def roman_to_int(s):
+    roman_numerals = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    
+    total = 0
+    prev_value = 0
+
+    for char in s:
+        value = roman_numerals[char]
+
+        if prev_value < value:
+            total += value - 2 * prev_value  
+        else:
+            total += value
+
+        prev_value = value
+
+    return total
+
+roman_num = input("Enter Roman Number")
+print(roman_to_int(roman_num)) 
+
